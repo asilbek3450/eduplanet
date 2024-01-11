f7ec39654b485a9856fd7b90ddd136f65234053e
asilbek <asilbekmirolimov@gmail.com>
2024-01-11 13:12:02 +0500

    changes

---
+++ apps/__init__.py
@@ -0,0 +1,3 @@
+from . import models
+from . import forms
+from . import views
--- apps/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import LearningCenter, Courses
-
-# Register your models here.
-admin.site.register(LearningCenter)
-admin.site.register(Courses)
